f5e4769c6ec956a553fba01224eb14f2f4b96e59
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-05-03 21:53:23 +0200

    Feat: add new logo

--- src/presentation/components/navbar/FalloutGuy.tsx
+++ src/presentation/components/navbar/FalloutGuy.tsx
@@ -1,11 +1,11 @@
-import fallout from '../../../assets/images/fallout.svg';
+import fallout from '../../../assets/images/logo_white.svg';
 
 const FalloutGuy = () => {
   return (
     <img
       src={fallout}
       alt="not found"
-      className="hidden lg:block w-12 filter-green"
+      className="hidden lg:block w-52 "
     />
   );
 };
