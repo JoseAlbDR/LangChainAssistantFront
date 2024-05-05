6c2fe6db276058b169576de47f0bdd4f0c751378
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-05-05 23:52:25 +0200

    fat: apply styles

--- src/presentation/components/config-modal/components/ApiKeyInput.tsx
+++ src/presentation/components/config-modal/components/ApiKeyInput.tsx
@@ -9,6 +9,7 @@ const ApiKeyInput = ({ register, errors }: ConfigInputProps) => {
         endContent={<Icon type={'key'} />}
         label="OpenAI API Key"
         variant="bordered"
+        className=" text-primary sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
         type="password"
         {...register('openAIApiKey')}
       />
