f0b44a88592872206605c95374a1e85e0514d4f8
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-05-05 23:48:34 +0200

    feat: add light / dark variant logos

---
+++ src/presentation/components/navbar/FalloutGuyDark.tsx
@@ -0,0 +1,13 @@
+import fallout from '../../../assets/images/logo_black.svg';
+
+const FalloutGuyDark = () => {
+  return (
+    <img
+      src={fallout}
+      alt="chatBot logo image"
+      className="hidden lg:block w-52 "
+    />
+  );
+};
+
+export default FalloutGuyDark;
--- src/presentation/components/navbar/FalloutGuyLight.tsx
+++ src/presentation/components/navbar/FalloutGuyLight.tsx
@@ -1,13 +1,13 @@
 import fallout from '../../../assets/images/logo_white.svg';
 
-const FalloutGuy = () => {
+const FalloutGuyLight = () => {
   return (
     <img
       src={fallout}
-      alt="not found"
+      alt="chatBot logo image"
       className="hidden lg:block w-52 "
     />
   );
 };
 
-export default FalloutGuy;
+export default FalloutGuyLight;
