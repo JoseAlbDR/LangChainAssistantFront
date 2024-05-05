37fc2f296dba17f6654fc9c24b1fb271b99602da
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-05-05 23:48:59 +0200

    Feat: apply styles

--- src/presentation/components/chat-bubbles/GptMessage.tsx
+++ src/presentation/components/chat-bubbles/GptMessage.tsx
@@ -1,5 +1,6 @@
 import Markdown from 'react-markdown';
 import { TypingLoader } from '..';
+import { IconMessageChatbot } from '@tabler/icons-react';
 
 interface Props {
   text: string;
@@ -9,13 +10,14 @@ const GptMessage = ({ text }: Props) => {
   return (
     <div className="col-start-1 sm:col-end-9 col-end-12 p-3 rounded-lg ">
       <div className="flex flex-row items-start">
-        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shrink-0 text-white">
-          <i className="fa fa-robot"></i>
+
+        <div className="flex items-center justify-center flex-shrink-0 text-white">
+          <IconMessageChatbot stroke={1} className='stroke-primary size-10' />
         </div>
         {text === '' ? (
           <TypingLoader />
         ) : (
-          <div className="relative ml-3 text-medium bg-primary bg-opacity-25 pt-3 pb-2 px-4 shadow-xl rounded-xl">
+          <div className="relative ml-3 text-medium bg-black bg-opacity-15 pt-3 pb-2 px-4 rounded-md">
             <Markdown>{text}</Markdown>
           </div>
         )}
--- src/presentation/components/chat-bubbles/UserMessage.tsx
+++ src/presentation/components/chat-bubbles/UserMessage.tsx
@@ -1,3 +1,5 @@
+import { IconUser } from "@tabler/icons-react";
+
 interface Props {
   text: string;
 }
@@ -6,10 +8,8 @@ const MyMessage = ({ text }: Props) => {
   return (
     <div className="sm:col-start-5 col-start-1 col-end-13 p-3 rounded-lg">
       <div className="flex items-center justify-start flex-row-reverse">
-        <div className="flex items-center justify-center h-10 w-10 rounded-full bg-tertiary-500 flex-shrink-0 text-white">
-          U
-        </div>
-        <div className="relative mr-3 text-medium bg-tertiary py-2 px-4 rounded-xl text-white shadow-xl">
+        <IconUser stroke={1} className='stroke-primary size-10' />
+        <div className="relative ml-3 text-medium bg-black bg-opacity-15 pt-3 pb-2 px-4 rounded-md">
           <span className="break-all">{text}</span>
         </div>
       </div>
