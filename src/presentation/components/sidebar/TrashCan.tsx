2f52e596bccb39d2ba9742e6c03b1fa2dcf3fa15
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-05-05 23:52:12 +0200

    feat: change colors

--- src/presentation/components/sidebar/TrashCan.tsx
+++ src/presentation/components/sidebar/TrashCan.tsx
@@ -1,7 +1,7 @@
 const TrashCan = () => {
   return (
     <div className="flex justify-center items-center rounded-md">
-      <span className="fa-solid fa-trash text-xl text-red-600" />
+      <span className="fa-solid fa-trash text-xl text-foreground" />
     </div>
   );
 };
